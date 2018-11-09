7fa811215d88ae2886a5851142d9a21ac9710000
David Joel McGarvey <davidjmcgarvey@gmail.com>
2018-11-09 14:38:09 -0800

    split orangize_players funtion into two functions
    two functions being yes_ex_player and no_ex_player

--- league_builder.py
+++ league_builder.py
@@ -1,27 +1,46 @@
 import csv
 
-
+# function to add player to separate file teams.txt
 def add_player(player):
 	# open file
 	file = open('teams.txt', 'a')
 	# write player to file
-	file.write(player)
+	file.write(player+'\n')
 	# close file
 	file.close()
 
 
-def organize_players():
+def yes_ex_players():
+	# read csv file with player information
 	with open('soccer_players.csv', newline='') as csvfile:
-		player_reader = csv.DictReader(csvfile, delimiter=',')
-		rows = list(player_reader)
+		# store player infomation in a variable
+		player_list = csv.DictReader(csvfile, delimiter=',')
+		# convert player info into a list to be iterated through
+		rows = list(player_list)
+		# iterate 
 		for row in rows:
+			# if player is a YES to having soccer experience,
 			if row['Soccer Experience'] == 'YES':
-				player_yes = row['Name'] + ', ' + row['Soccer Experience'] + ', ' + row['Guardian Name(s)'] + '\n'
+				# get desired info, concatenate it, store it to variable
+				add_player(row['Name'] + ', ' + row['Soccer Experience'] + ', ' + row['Guardian Name(s)'])
 			else:
-				player_no = row['Name'] + ', ' + row['Soccer Experience'] + ', ' + row['Guardian Name(s)']
-		add_player(player_yes)
-		add_player(player_no)
-	
-organize_players()
-			
+				pass
 
+
+def no_ex_players():
+	# read csv file with player information
+	with open('soccer_players.csv', newline='') as csvfile:
+		# store player infomation in a variable
+		player_list = csv.DictReader(csvfile, delimiter=',')
+		# convert player info into a list to be iterated through
+		rows = list(player_list)
+		# iterate
+		for row in rows:
+			# if player is a YES to having soccer experience,
+			if row['Soccer Experience'] == 'NO':
+				add_player(row['Name'] + ', ' + row['Soccer Experience'] + ', ' + row['Guardian Name(s)'])
+			else:
+				pass
+
+yes_ex_players()
+no_ex_players()
