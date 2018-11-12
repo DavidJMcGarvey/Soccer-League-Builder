69184f9cb30945652a466dae179b86717454cff7
David Joel McGarvey <davidjmcgarvey@gmail.com>
2018-11-12 10:35:21 -0800

    Adjust slices and change variable names

--- league_builder.py
+++ league_builder.py
@@ -1,6 +1,124 @@
 import csv
 
 
+# function to add player to separate file teams.txt
+def add_player(name):
+	# open file
+	file = open('teams.txt', 'a')
+	# write player to file
+	file.write(name + '\n')
+	# close file
+	file.close()
+
+
+def sharks_create():
+	# read csv file with player information
+	with open('soccer_players.csv', newline='') as csvfile:
+		# store player information in a variable
+		player_list = csv.DictReader(csvfile, delimiter=',')
+		# convert player info into a list to be iterated through
+		players = list(player_list)
+		# iterate (PORTION of) list...
+		for player in players[:3]:
+			# if player is a YES to having soccer experience,
+			if player['Soccer Experience'] == 'YES':
+				# concatenate desired player info and write it to file using add_player
+				add_player(player['Name'] + ', ' + player['Soccer Experience'] + ', ' + player['Guardian Name(s)'])
+		for player in players[:6]:
+			if player['Soccer Experience'] == 'NO':
+				# concatenate desired player info and write it to file using add_player
+				add_player(player['Name'] + ', ' + player['Soccer Experience'] + ', ' + player['Guardian Name(s)'])
+
+
+def sharks_note():
+	shark_practice = "Saturday at 9:00am"
+	# read csv file with player information
+	with open('soccer_players.csv', newline='') as csvfile:
+		player_list = csv.DictReader(csvfile, delimiter=',')
+		players = list(player_list)
+		for player in players[:6]:
+			player_name = to_snake_case(player['Name'])
+			file = open('{}.txt'.format(player_name), 'a')
+			file.write('Dear ' + player['Guardian Name(s)'] + ',\nYour child, ' + player['Name'] + ', has their first practice on ' + shark_practice + '. Thanks for joining our soccer league!')
+
+
+def raptors_create():
+	# read csv file with player information
+	with open('soccer_players.csv', newline='') as csvfile:
+		# store player information in a variable
+		player_list = csv.DictReader(csvfile, delimiter=',')
+		# convert player info into a list to be iterated through
+		players = list(player_list)
+		# iterate (PORTION of) list...
+		for player in players[6:13]:
+			# if player is a YES to having soccer experience,
+			if player['Soccer Experience'] == 'YES':
+				# concatenate desired player info and write it to file using add_player
+				add_player(player['Name'] + ', ' + player['Soccer Experience'] + ', ' + player['Guardian Name(s)'])
+		for player in players[6:11]:
+			if player['Soccer Experience'] == 'NO':
+				# concatenate desired player info and write it to file using add_player
+				add_player(player['Name'] + ', ' + player['Soccer Experience'] + ', ' + player['Guardian Name(s)'])
+
+
+def raptors_note():
+	raptor_practice = "Saturday at 10:00am"
+	# read csv file with player information
+	with open('soccer_players.csv', newline='') as csvfile:
+		player_list = csv.DictReader(csvfile, delimiter=',')
+		players = list(player_list)
+		for player in players[6:11]:
+			player_name = to_snake_case(player['Name'])
+			file = open('{}.txt'.format(player_name), 'a')
+			file.write('Dear ' + player['Guardian Name(s)'] + ',\nYour child, ' + player['Name'] + ', has their first practice on ' + raptor_practice + '. Thanks for joining our soccer league!')
+		for player in players[12:13]:
+			player_name = to_snake_case(player['Name'])
+			file = open('{}.txt'.format(player_name), 'a')
+			file.write('Dear ' + player['Guardian Name(s)'] + ',\nYour child, ' + player['Name'] + ', has their first practice on ' + raptor_practice + '. Thanks for joining our soccer league!')
+
+
+def dragons_create():
+	# read csv file with player information
+	with open('soccer_players.csv', newline='') as csvfile:
+		# store player information in a variable
+		player_list = csv.DictReader(csvfile, delimiter=',')
+		# convert player info into a list to be iterated through
+		players = list(player_list)
+		# iterate (PORTION of) list...
+		for player in players[13:]:
+			# if player is a YES to having soccer experience,
+			if player['Soccer Experience'] == 'YES':
+				# concatenate desired player info and write it to file using add_player
+				add_player(player['Name'] + ', ' + player['Soccer Experience'] + ', ' + player['Guardian Name(s)'])
+		for player in players[11:]:
+			if player['Soccer Experience'] == 'NO':
+				# concatenate desired player info and write it to file using add_player
+				add_player(player['Name'] + ', ' + player['Soccer Experience'] + ', ' + player['Guardian Name(s)'])
+
+
+def dragons_note():
+	dragon_practice = "Saturday at 11:00am"
+	# read csv file with player information
+	with open('soccer_players.csv', newline='') as csvfile:
+		player_list = csv.DictReader(csvfile, delimiter=',')
+		players = list(player_list)
+		for player in players[11:12]:
+			player_name = to_snake_case(player['Name'])
+			file = open('{}.txt'.format(player_name), 'a')
+			file.write('Dear ' + player['Guardian Name(s)'] + ',\nYour child, ' + player['Name'] + ', has their first practice on ' + dragon_practice + '. Thanks for joining our soccer league!')
+		for player in players[14:]:
+			player_name = to_snake_case(player['Name'])
+			file = open('{}.txt'.format(player_name), 'a')
+			file.write('Dear ' + player['Guardian Name(s)'] + ',\nYour child, ' + player['Name'] + ', has their first practice on ' + dragon_practice + '. Thanks for joining our soccer league!')
+
+
+# function to create team name and insert in teams.txt
+def create_team(team):
+	file = open('teams.txt', 'a')
+	file.write(team + '\n')
+
+
+# function to convert player name to lower and snake_case for title of 'Welcome' letters
 def to_snake_case(name):
 	final = ''
 	for i in range(len(name)):
@@ -19,100 +137,14 @@ def to_snake_case(name):
 		final = final[1:]
 	return final
 
-
-# function to add player to separate file teams.txt
-def add_player(player):
-	# open file
-	file = open('teams.txt', 'a')
-	# write player to file
-	file.write(player + '\n')
-	# close file
-	file.close()
-
-
-def add_note_sharks():
-	shark_practice = "Saturday at 9:00am"
-	# raptor_practice = "Saturday at 10:00am"
-	# dragon_practice = "Saturday at 11:00am"
-	with open('soccer_players.csv', newline='') as csvfile:
-		player_list = csv.DictReader(csvfile, delimiter=',')
-		rows = list(player_list)
-		for row in rows[:6]:
-			player_name = to_snake_case(row['Name'])
-			file = open('{}.txt'.format(player_name), 'a')
-			file.write('Dear ' + row['Guardian Name(s)'] + ',\nYour child, ' + row['Name'] + ', has their first practice on ' + shark_practice + '. Thanks for joining our soccer league!')
-
-
-def create_sharks():
-	# read csv file with player information
-	with open('soccer_players.csv', newline='') as csvfile:
-		# store player information in a variable
-		player_list = csv.DictReader(csvfile, delimiter=',')
-		# convert player info into a list to be iterated through
-		rows = list(player_list)
-		# iterate list
-		for row in rows[:3]:
-			# if player is a YES to having soccer experience,
-			if row['Soccer Experience'] == 'YES':
-				# concatenate desired player info and write it to file using add_player
-				add_player(row['Name'] + ', ' + row['Soccer Experience'] + ', ' + row['Guardian Name(s)'])
-		for row in rows[:6]:
-			if row['Soccer Experience'] == 'NO':
-				# concatenate desired player info and write it to file using add_player
-				add_player(row['Name'] + ', ' + row['Soccer Experience'] + ', ' + row['Guardian Name(s)'])
-
-
-def create_raptors():
-	# read csv file with player information
-	with open('soccer_players.csv', newline='') as csvfile:
-		# store player information in a variable
-		player_list = csv.DictReader(csvfile, delimiter=',')
-		# convert player info into a list to be iterated through
-		rows = list(player_list)
-		# iterate (PORTION OF) list
-		for row in rows[6:13]:
-			# if player is a YES to having soccer experience,
-			if row['Soccer Experience'] == 'YES':
-				# concatenate desired player info and write it to file using add_player
-				add_player(row['Name'] + ', ' + row['Soccer Experience'] + ', ' + row['Guardian Name(s)'])
-		for row in rows[9:12]:
-			if row['Soccer Experience'] == 'NO':
-				# concatenate desired player info and write it to file using add_player
-				add_player(row['Name'] + ', ' + row['Soccer Experience'] + ', ' + row['Guardian Name(s)'])
-
-
-def create_dragons():
-	# read csv file with player information
-	with open('soccer_players.csv', newline='') as csvfile:
-		# store player information in a variable
-		player_list = csv.DictReader(csvfile, delimiter=',')
-		# convert player info into a list to be iterated through
-		rows = list(player_list)
-		# iterate list
-		for row in rows[13:]:
-			# if player is a YES to having soccer experience,
-			if row['Soccer Experience'] == 'YES':
-				# concatenate desired player info and write it to file using add_player
-				add_player(row['Name'] + ', ' + row['Soccer Experience'] + ', ' + row['Guardian Name(s)'])
-		for row in rows[13:]:
-			if row['Soccer Experience'] == 'NO':
-				# concatenate desired player info and write it to file using add_player
-				add_player(row['Name'] + ', ' + row['Soccer Experience'] + ', ' + row['Guardian Name(s)'])
-
-
-def create_team(team):
-	file = open('teams.txt', 'a')
-	file.write(team + '\n')
-
-
-# Team names: Sharks, Raptors, Dragons
-# !! How do I use the two functions that split the players into 3 teams?
-# 3 players from each group (yes_ex and no_ex) per team
+# to make sure script isn't executed when imported
 if __name__ == '__main__':
-    add_note_sharks()
-	# create_team('Sharks')
-	# create_sharks()
-	# create_team('Raptors')
-	# create_raptors()
-	# create_team('Dragons')
-	# create_raptors()
+	create_team('Sharks')
+	sharks_create()
+	sharks_note()
+	create_team('Raptors')
+	raptors_create()
+	raptors_note()
+	create_team('Dragons')
+	dragons_create()
+	dragons_note()
